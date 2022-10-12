054053be24ed718a7b6c97244f9d0120d429f2c6
Thomas Tresansky <ttresansky@gradle.com>
2022-10-12 10:06:55 -0400

    Remove deprecated getUsage method

--- subprojects/core/src/main/java/org/gradle/api/internal/component/UsageContext.java
+++ subprojects/core/src/main/java/org/gradle/api/internal/component/UsageContext.java
@@ -16,10 +16,6 @@
 
 package org.gradle.api.internal.component;
 
-import org.gradle.api.attributes.Usage;
 import org.gradle.api.component.SoftwareComponentVariant;
 
-public interface UsageContext extends SoftwareComponentVariant {
-    @Deprecated
-    Usage getUsage(); // kept for backwards compatibility of plugins (like kotlin-multiplatform) using internal APIs
-}
+public interface UsageContext extends SoftwareComponentVariant {}
