4206ccbc565daeb509137e8bccd19c4ad7ef8f15
Bo Zhang <bo@gradle.com>
2023-05-17 10:54:36 +0800

    simple reproducer

--- .teamcity/src/main/kotlin/projects/GradleBuildToolRootProject.kt
+++ .teamcity/src/main/kotlin/projects/GradleBuildToolRootProject.kt
@@ -1,5 +1,6 @@
 package projects
 
+import jetbrains.buildServer.configs.kotlin.v2019_2.DslContext
 import jetbrains.buildServer.configs.kotlin.v2019_2.Project
 import jetbrains.buildServer.configs.kotlin.v2019_2.buildSteps.gradle
 
@@ -7,6 +8,9 @@ class GradleBuildToolRootProject() : Project({
     buildType {
         id("reproducer")
         name = "reproducer"
+        vcs {
+            root(DslContext.settingsRoot)
+        }
         requirements {
             contains("teamcity.agent.jvm.os.name", "Linux")
         }
