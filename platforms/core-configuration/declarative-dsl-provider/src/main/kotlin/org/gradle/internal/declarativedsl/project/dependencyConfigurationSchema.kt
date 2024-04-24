8d792b5119292aba2a2fc70f5224418857bf0196
József Bartók <jbartok@gradle.com>
2024-04-24 11:26:01 +0300

    Transform public interfaces back to Kotlin

--- platforms/core-configuration/declarative-dsl-provider/src/main/kotlin/org/gradle/internal/declarativedsl/project/dependencyConfigurationSchema.kt
+++ platforms/core-configuration/declarative-dsl-provider/src/main/kotlin/org/gradle/internal/declarativedsl/project/dependencyConfigurationSchema.kt
@@ -23,11 +23,10 @@ import org.gradle.api.artifacts.dsl.DependencyHandler
 import org.gradle.declarative.dsl.schema.DataConstructor
 import org.gradle.declarative.dsl.schema.DataParameter
 import org.gradle.declarative.dsl.schema.DataTopLevelFunction
-import org.gradle.declarative.dsl.schema.FunctionSemantics.ConfigureBlockRequirement.NOT_ALLOWED
 import org.gradle.declarative.dsl.schema.SchemaMemberFunction
 import org.gradle.internal.declarativedsl.analysis.DataMemberFunction
 import org.gradle.internal.declarativedsl.analysis.DefaultDataParameter
-import org.gradle.internal.declarativedsl.analysis.FunctionSemanticsInternal
+import org.gradle.internal.declarativedsl.analysis.DefaultFunctionSemantics
 import org.gradle.internal.declarativedsl.analysis.ParameterSemanticsInternal
 import org.gradle.internal.declarativedsl.analysis.ParameterValueBinding
 import org.gradle.internal.declarativedsl.evaluationSchema.EvaluationSchemaComponent
@@ -93,7 +92,7 @@ class DependencyFunctionsExtractor(private val configurations: DependencyConfigu
                     configurationName,
                     listOf(projectDependencyParam),
                     false,
-                    FunctionSemanticsInternal.AddAndConfigure(ProjectDependency::class.toDataTypeRef(), NOT_ALLOWED)
+                    DefaultFunctionSemantics.DefaultAddAndConfigure(ProjectDependency::class.toDataTypeRef(), DefaultFunctionSemantics.DefaultConfigureBlockRequirement.DefaultNotAllowed)
                 )
             }
         } else emptyList()
