tianyeeT/gradle

Introduce strong types for InternalProblemSpec.additionalData

platforms/core-runtime/logging/src/main/java/org/gradle/internal/featurelifecycle/LoggingDeprecatedFeatureHandler.java
@@ -21,6 +21,8 @@ import org.gradle.api.GradleException;
 import org.gradle.api.internal.DocumentationRegistry;
 import org.gradle.api.logging.configuration.WarningMode;
 import org.gradle.api.problems.Problems;
+import org.gradle.api.problems.internal.DeprecationAdditionalDataSpec;
+import org.gradle.api.problems.internal.DeprecationData;
 import org.gradle.api.problems.internal.GradleCoreProblemGroup;
 import org.gradle.api.problems.internal.InternalProblemReporter;
 import org.gradle.api.problems.internal.InternalProblemSpec;
@@ -103,7 +105,12 @@ public class LoggingDeprecatedFeatureHandler implements FeatureHandler<Deprecate
                     .contextualLabel(usage.getSummary())
                     .details(usage.getRemovalDetails())
                     .documentedAt(usage.getDocumentationUrl())
-                    .additionalData("type", usage.getType().name()) // TODO we should use a custom type here
+                    .additionalData(DeprecationData.class, new Action<DeprecationAdditionalDataSpec>() {
+                        @Override
+                        public void execute(DeprecationAdditionalDataSpec spec) {
+                            spec.type(DeprecationData.Type.valueOf(usage.getType().name()));
+                        }
+                    })
                     .severity(WARNING);
 
                 addPossibleLocation(diagnostics, problemSpec);

vimanthan3/wed03

platforms/core-runtime/logging/src/main/java/org/gradle/internal/featurelifecycle/LoggingDeprecatedFeatureHandler.java
@@ -21,6 +21,8 @@ import org.gradle.api.GradleException;
 import org.gradle.api.internal.DocumentationRegistry;
 import org.gradle.api.logging.configuration.WarningMode;
 import org.gradle.api.problems.Problems;
+import org.gradle.api.problems.internal.DeprecationData;
+import org.gradle.api.problems.internal.DeprecationDataSpec;
 import org.gradle.api.problems.internal.GradleCoreProblemGroup;
 import org.gradle.api.problems.internal.InternalProblemReporter;
 import org.gradle.api.problems.internal.InternalProblemSpec;
@@ -103,7 +105,12 @@ public class LoggingDeprecatedFeatureHandler implements FeatureHandler<Deprecate
                     .contextualLabel(usage.getSummary())
                     .details(usage.getRemovalDetails())
                     .documentedAt(usage.getDocumentationUrl())
-                    .additionalData("type", usage.getType().name()) // TODO we should use a custom type here
+                    .additionalData(DeprecationData.class, new Action<DeprecationDataSpec>() {
+                        @Override
+                        public void execute(DeprecationDataSpec spec) {
+                            spec.type(DeprecationData.Type.valueOf(usage.getType().name()));
+                        }
+                    })
                     .severity(WARNING);
 
                 addPossibleLocation(diagnostics, problemSpec);