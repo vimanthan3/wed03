vmadalin/gradle

Add JavaToolchainResolverRegistry to updateDaemonJvm task to generate download URLs on the build properties
Signed-off-by: Madalin Valceleanu <vmadalin@google.com>

platforms/core-runtime/build-configuration/build.gradle.kts
@@ -15,8 +15,10 @@ dependencies {
     api(project(":toolchains-jvm-shared"))
 
     implementation(project(":base-annotations"))
+    implementation(project(":platform-jvm"))
 
     testImplementation(testFixtures(project(":core")))
+    testImplementation(testFixtures(project(":toolchains-jvm-shared")))
 
     testFixturesImplementation(project(":core-api"))
 

vimanthan3/wed03

platforms/core-runtime/build-configuration/build.gradle.kts
@@ -11,12 +11,14 @@ dependencies {
     api(project(":base-services"))
     api(project(":core"))
     api(project(":core-api"))
-    api(project(":jvm-services"))
     api(project(":toolchains-jvm-shared"))
     api(project(":base-annotations"))
+    api(project(":platform-jvm"))
     implementation(project(":logging"))
+    implementation(project(":jvm-services"))
 
     testImplementation(testFixtures(project(":core")))
+    testImplementation(testFixtures(project(":toolchains-jvm-shared")))
 
     testFixturesImplementation(project(":core-api"))
     testFixturesImplementation(project(":internal-integ-testing"))